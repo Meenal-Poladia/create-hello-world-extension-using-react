f97369e7c8825253ee50e769476ddd62a5a46600
Meenal-Poladia <meenal.poladia94@gmail.com>
2022-02-21 11:25:50 +0530

    Adding checkbox, updating CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const handleSubmit = (e) => {
+        console.log(e.target.checked)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Hello World</h3>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+                type="checkbox"
+                name="changeText"
+                id="changeText"
+                onClick={(e) => handleSubmit(e)}
+            />
+            <label htmlFor="changeText">Check box</label>
+        </form>
+
     </div>
   );
 }
